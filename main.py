d13a362f1b86057660a96c09a193c89b3838b8ec
Philip Krause <pkrause@exzeo.com>
2023-07-26 17:53:28 -0500

    Changes unrelated to a JIRA task

--- main.py
+++ main.py
@@ -1,38 +1,28 @@
+import matplotlib.pyplot as plt
 import numpy as np
-
-
-from network.dot import dotProduct
-from network.product import make_prediction,sigmoid_deriv,sigmoid
-
-
-#Vars
-target = 0
-input_vector = np.array([2, 1.5])
-weights_1 = np.array([1.45, -0.66])
-bias = np.array([0.0])
-
-# Prediction
-prediction = make_prediction(input_vector,weights_1,bias) 
-# Mean Squared Error
-mse = np.square(prediction - target)  
-# Derivitve
-derivative = 2 * (prediction - target) 
-#Adjust the weight with the derivative
-weights_1 = weights_1 - derivative 
-
-error = (prediction - target) ** 2
-
-print(f"The derivative is {derivative}")
-print(f"Prediction: {prediction}; Error: {error}")
-
-
-#Using backpropagation 
-derror_dprediction = 2 * (prediction - target)
-layer_1 = np.dot(input_vector, weights_1) + bias
-dprediction_dlayer1 = sigmoid_deriv(layer_1)
-dlayer1_dbias = 1
-
-
-derror_dbias = (
-    derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+from network.neural import NeuralNetwork 
+input_vectors = np.array(
+    [
+        [3, 1.5],
+        [2, 1],
+        [4, 1.5],
+        [3, 4],
+        [3.5, 0.5],
+        [2, 0.5],
+        [5.5, 1],
+        [1, 1],
+    ]
 )
+
+targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
+
+learning_rate = 0.1
+
+neural_network = NeuralNetwork(learning_rate)
+
+training_error = neural_network.train(input_vectors, targets, 10000)
+
+plt.plot(training_error)
+plt.xlabel("Iterations")
+plt.ylabel("Error for all training instances")
+plt.savefig("cumulative_error.png")
--- network/neural.py
+++ network/neural.py
@@ -16,13 +16,14 @@ class NeuralNetwork:
     def _sigmoid_deriv(self, x):
         return self._sigmoid(x) * (1 - self._sigmoid(x))
 
-    #
+    #use dot product to determine the difference in vectors
     def predict(self, input_vector):
         layer_1 = np.dot(input_vector, self.weights) + self.bias
         layer_2 = self._sigmoid(layer_1)
         prediction = layer_2
         return prediction
 
+    #With the chain rule, you take the partial derivatives of each function, evaluate them, and multiply all the partial derivatives to get the derivative you want.
     def _compute_gradients(self, input_vector, target):
         layer_1 = np.dot(input_vector, self.weights) + self.bias
         layer_2 = self._sigmoid(layer_1)
@@ -47,3 +48,35 @@ class NeuralNetwork:
         self.weights = self.weights - (
             derror_dweights * self.learning_rate
         )
+        
+    def train(self, input_vectors, targets, iterations):
+            cumulative_errors = []
+            for current_iteration in range(iterations):
+                # Pick a data instance at random
+                random_data_index = np.random.randint(len(input_vectors))
+
+                input_vector = input_vectors[random_data_index]
+                target = targets[random_data_index]
+
+                # Compute the gradients and update the weights
+                derror_dbias, derror_dweights = self._compute_gradients(
+                    input_vector, target
+                )
+
+                self._update_parameters(derror_dbias, derror_dweights)
+
+                # Measure the cumulative error for all the instances
+                if current_iteration % 100 == 0:
+                    cumulative_error = 0
+                    # Loop through all the instances to measure the error
+                    for data_instance_index in range(len(input_vectors)):
+                        data_point = input_vectors[data_instance_index]
+                        target = targets[data_instance_index]
+
+                        prediction = self.predict(data_point)
+                        error = np.square(prediction - target)
+
+                        cumulative_error = cumulative_error + error
+                    cumulative_errors.append(cumulative_error)
+
+            return cumulative_errors
